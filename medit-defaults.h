20e7b7ec70a6edde43dd5007f3770d1fa75bb52b
k n z z <34898147+theknzz@users.noreply.github.com>
2018-10-24 19:01:41 +0100

    Add files via upload

--- medit-defaults.h
+++ medit-defaults.h
@@ -1,26 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ncurses.h>
-#include <unistd.h>
-#include <pthread.h>
+#ifndef MEDIT
+#define MEDIT
 
-typedef struct {
-    int n_utilizadores, n_palavras, n_letras;
-    char fich_utilizadores[15], chars_mais_comuns[5];
-} servidor;
 
-typedef struct {
-    char nome[50];
-    int tempo_linha, linhas_editadas, linha_atual;
-} utilizador;
 
-typedef struct {
-    int tipo;
-    char aviso[50];
-} avisos;
-
-typedef struct {
-    int nlinhas, ncolunas;
-    int posx, posy;
-} editor;
+#endif
