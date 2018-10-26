d956805123b47fa43da9550005dccc7297fde64a
João Silva <44022149+Lato17@users.noreply.github.com>
2018-10-26 20:03:47 +0100

    Add files via upload

--- client.c
+++ client.c
@@ -2,7 +2,8 @@
 #include "client.h"
 #include "server.h"
 
-void criar_editor(){
+void criar_editor()
+{
     int tecla;
     editor t;
     char tab[t.nlinhas][t.ncolunas];
@@ -13,7 +14,7 @@ void criar_editor(){
     noecho();
     cbreak();
 
-    janela = newwin(17,47, 2, 2);
+    janela = newwin(17, 47, 2, 2);
 
     box(janela, ACS_VLINE, ACS_HLINE);
 
@@ -24,50 +25,93 @@ void criar_editor(){
     t.c_atual = 1;
 
     wmove(janela, t.l_atual, t.c_atual);
-    
+
     wrefresh(janela);
-    while(1){
+    while (1)
+    {
         tecla = wgetch(janela);
         getyx(janela, t.l_atual, t.c_atual);
-        switch(tecla){
+        switch (tecla)
+        {
         case KEY_UP:
-            if(t.l_atual == 1)
+            if (t.l_atual == 1)
                 break;
             wmove(janela, t.l_atual - 1, t.c_atual);
-            wrefresh(janela);
-        break;
+            break;
         case KEY_DOWN:
-            if(t.l_atual == 15)
+            if (t.l_atual == 15)
                 break;
             wmove(janela, t.l_atual + 1, t.c_atual);
-            wrefresh(janela);
-        break;
+            break;
         case KEY_RIGHT:
-            if(t.c_atual == 45)
+            if (t.c_atual == 45)
                 break;
             wmove(janela, t.l_atual, t.c_atual + 1);
-            wrefresh(janela);
-        break;
+            break;
         case KEY_LEFT:
-            if(t.c_atual == 1)
+            if (t.c_atual == 1)
                 break;
             wmove(janela, t.l_atual, t.c_atual - 1);
+            break;
+        case 10:
+            echo();
             wrefresh(janela);
-        break;
+            while (1)
+            {
+                tecla = wgetch(janela);
+                getyx(janela, t.l_atual, t.c_atual);
+                switch (tecla)
+                {
+                case KEY_RIGHT:
+                    if (t.c_atual == 45)
+                        break;
+                    wmove(janela, t.l_atual, t.c_atual + 1);
+                    break;
+                case KEY_LEFT:
+                    if (t.c_atual == 1)
+                        break;
+                    wmove(janela, t.l_atual, t.c_atual - 1);
+                    break;
+
+                }
+                if (tecla == 27)
+                    {
+                        //descarta_info();
+                        noecho();
+                        break;
+                    }
+            }
+            tecla = 0;
+            break;
         }
-        if(tecla == 27)
+        if (tecla == 27)
             break;
     }
 
     wrefresh(janela);
     endwin();
-} 
+}
 
+/*int main(int argc, char *argv) {
+	char str[11], *pal[3];
+	int n, i;
+	fgets(str,12,stdin);
+	puts(str);
+	str[strlen(str)-1] = '\0';
+	n = 0;
+	pal[n] = strtok(str, " ");
+	n++;
+	while( (pal[n] = strtok( NULL, " ") ) != NULL)
+		n++;
+	for(i=0;i<n;i++)
+		printf("%s\n",pal[i]);
+	exit(0);
+}*/
 
-int main(int argc, char**argv){
-
-    //getUser(argc, argv, s);
-	criar_editor();
+int main(int argc, char **argv)
+{
+    //getUser(argc, argv);
+    criar_editor();
 
     return 0;
 }
