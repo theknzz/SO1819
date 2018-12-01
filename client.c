8e74cf0cbb8430bec62194d8efb07abc677cae26
João Silva <44022149+Lato17@users.noreply.github.com>
2018-12-01 20:09:54 +0000

    sinal

--- client.c
+++ client.c
@@ -2,26 +2,16 @@
 #include "client.h"
 #include "server.h"
 
-void criar_editor(WINDOW *janela, editor *t, char tab[t->nlinhas][t->ncolunas], char inter_fifo_fname[20])
+void criar_editor(WINDOW *janela, editor *t, char tab[t->nlinhas][t->ncolunas], char inter_fifo_fname[20], int c_fifo_fd)
 {
+    WINDOW *erros;
     int tecla, i = 0, j, coluna_ini = 2;
     int tecla2;
     char x, aux[t->ncolunas], res;
 
-    int c_fifo_fd, inter_fifo_fd;
+    int inter_fifo_fd;
     char c_fifo_fname[20];
     request req;
-    controlo ctrl;
-
-    // preenche o id do request
-    req.pid_cliente = getpid();
-    sprintf(c_fifo_fname, CLIENT_FIFO, req.pid_cliente);
-
-    // cria o pipe
-    res = mkfifo(c_fifo_fname, 0600);  
-    if (res == -1)
-        exit(EXIT_FAILURE);
-    
 
     initscr();     // inicializa o uso do ncurses
     start_color(); // inicia as cores
@@ -39,6 +29,8 @@ void criar_editor(WINDOW *janela, editor *t, char tab[t->nlinhas][t->ncolunas], 
 
     janela = newwin(t->nlinhas + 2, t->ncolunas + 3, 2, 3); //Criação da janela (linhas, colunas, posiçãoy no stdscr, posiçãox no stdscr)
 
+    erros = newwin(10, 30, 2, t->ncolunas + 6);
+
     box(janela, ' ', ACS_HLINE); //Criação do border (WINDOW, tipo de border, tipo de border)
 
     keypad(janela, TRUE); // Permite ler as letras do teclado (FALSE por default)
@@ -52,7 +44,7 @@ void criar_editor(WINDOW *janela, editor *t, char tab[t->nlinhas][t->ncolunas], 
 
     for (i = 0; i < t->nlinhas; i++)
     {
-        mvwprintw(janela, t->l_atual, t->c_atual, "%2d", (i + 1 ));
+        mvwprintw(janela, t->l_atual, t->c_atual, "%2d", i);
         if (t->l_atual == t->nlinhas)
             break;
         t->l_atual++;
@@ -76,9 +68,18 @@ void criar_editor(WINDOW *janela, editor *t, char tab[t->nlinhas][t->ncolunas], 
 
     wmove(janela, t->l_atual, t->c_atual); // Move o cursor da janela para uma posição da janela
 
+    wprintw(erros, "pipe de interacao atribuido: %s", inter_fifo_fname);
+    wrefresh(erros);
+
     wrefresh(janela); // Atualiza a janela
     while (1)
     {
+        if (access(SERVER_FIFO_P, F_OK) != 0)
+        {
+            wprintw(erros, "\nServidor nao existe...!\n");
+            wrefresh(erros);
+            break;
+        }
         tecla = wgetch(janela);                // Capta um caracter da janela e devolve o inteiro correspondente
         getyx(janela, t->l_atual, t->c_atual); // Atribui as coordenadas do cursor para as variáveis
         switch (tecla)
@@ -104,164 +105,168 @@ void criar_editor(WINDOW *janela, editor *t, char tab[t->nlinhas][t->ncolunas], 
             wmove(janela, t->l_atual, t->c_atual - 1);
             break;
         case 10: // No caso de ENTER
-            
+
             // abre o FIFO do servidor para escrita
             inter_fifo_fd = open(inter_fifo_fname, O_WRONLY);
-            if(inter_fifo_fd == -1) {
-                unlink(c_fifo_fname);
-                exit(EXIT_FAILURE);
-            }
-
-            // abre o FIFO do cliente para escrita e leitura
-            c_fifo_fd = open(c_fifo_fname, O_RDWR);
-            if(c_fifo_fd == -1) {
-                close(inter_fifo_fd);
+            if (inter_fifo_fd == -1)
+            {
                 unlink(c_fifo_fname);
                 exit(EXIT_FAILURE);
             }
 
             // linha selecionada e posta na struct request
             req.nr_linha = t->l_atual - 1;
-
-            // envia a struct request para o server        
+            req.pid_cliente = getpid();
+            req.perm = 0;
+            // envia a struct request para o server
             write(inter_fifo_fd, &req, sizeof(req));
 
             // le a resposta do servidor
-            read(c_fifo_fd, &ctrl, sizeof(ctrl));
+            read(c_fifo_fd, &req, sizeof(req));
 
-            if(ctrl.perm == 1)
+            if (req.perm == 1)
             {
-                    attron(COLOR_PAIR(2));
-                    mvwchgat(janela, t->l_atual, 0, 2, 0, 2, NULL);
-                    attroff(COLOR_PAIR(2));
-                    wmove(janela, t->l_atual, t->c_atual);
-                    wrefresh(janela);
+                attron(COLOR_PAIR(2));
+                mvwchgat(janela, t->l_atual, 0, 2, 0, 2, NULL);
+                attroff(COLOR_PAIR(2));
+                wmove(janela, t->l_atual, t->c_atual);
+                wrefresh(janela);
 
-                    // copiar a linha existente para uma linha aux
-                    for (i = 0; i < t->ncolunas; i++)
+                // copiar a linha existente para uma linha aux
+                for (i = 0; i < t->ncolunas; i++)
+                {
+                    aux[i] = tab[t->l_atual - 1][i];
+                }
+                // Enquanto estiver no modo de edicao
+                // e nao quiser sair ...
+                while (tecla2 != 27 && tecla == 10)
+                {
+                    if (access(SERVER_FIFO_P, F_OK) != 0)
                     {
-                        aux[i] = tab[t->l_atual - 1][i];
+                        wprintw(erros, "\nServidor nao existe...!\n");
+                        wrefresh(erros);
+                        break;
                     }
-                    // Enquanto estiver no modo de edicao
-                    // e nao quiser sair ...
-                    while (tecla2 != 27 && tecla == 10)
+                    getyx(janela, t->l_atual, t->c_atual);
+
+                    if (t->c_atual >= t->ncolunas + 2)
+                        wmove(janela, t->l_atual, t->c_atual - 1);
+                    else if (t->c_atual < 2)
+                        wmove(janela, t->l_atual, t->c_atual + 1);
+                    else if (t->c_atual < 1)
+                        wmove(janela, t->l_atual, t->c_atual + 2);
+
+                    tecla2 = wgetch(janela);
+
+                    switch (tecla2)
                     {
-                        getyx(janela, t->l_atual, t->c_atual);
+                    case KEY_STAB:
+                        break;
+                    case KEY_UP:
+                        break;
+                    case KEY_DOWN:
+                        break;
+                    case KEY_RIGHT:
+                        if (t->c_atual == t->ncolunas + 1)
+                            break;
+                        wmove(janela, t->l_atual, t->c_atual + 1);
+                        break;
 
-                        if (t->c_atual >= t->ncolunas + 2)
-                            wmove(janela, t->l_atual, t->c_atual - 1);
-                        else if (t->c_atual < 2)
-                            wmove(janela, t->l_atual, t->c_atual + 1);
-                        else if (t->c_atual < 1)
-                            wmove(janela, t->l_atual, t->c_atual + 2);
+                    case KEY_LEFT:
+                        if (t->c_atual == 2)
+                            break;
+                        wmove(janela, t->l_atual, t->c_atual - 1);
+                        break;
 
-                        tecla2 = wgetch(janela);
-                        
-                        switch (tecla2)
+                    case KEY_BACKSPACE:
+                        if (t->c_atual > 2)
                         {
-                        case KEY_STAB:
-                            break;
-                        case KEY_UP:
-                            break;
-                        case KEY_DOWN:
-                            break;
-                        case KEY_RIGHT:
-                            if (t->c_atual == t->ncolunas + 1)
-                                break;
-                            wmove(janela, t->l_atual, t->c_atual + 1);
-                        break;
-                        
-                        case KEY_LEFT:
-                            if (t->c_atual == 2)
-                                break;
-                            wmove(janela, t->l_atual, t->c_atual - 1);
-                        break;
-                        
-                        case KEY_BACKSPACE: 
-                            if (t->c_atual > 2)
-                            {
-                                for (i = t->c_atual - 2; i < t->ncolunas; i++)
-                                {
-                                    aux[i - 1] = aux[i];
-                                }
-                                aux[t->ncolunas - 1] = ' ';
-                                for (i = 0; i < t->ncolunas; i++)
-                                {
-                                    mvwprintw(janela, t->l_atual, i + 2, "%c", aux[i]);
-                                }
-                                wmove(janela, t->l_atual, t->c_atual - 1);
-                            }
-                        break;
-                        
-                        case KEY_DC:
                             for (i = t->c_atual - 2; i < t->ncolunas; i++)
                             {
-                                aux[i] = aux[i + 1];
+                                aux[i - 1] = aux[i];
                             }
                             aux[t->ncolunas - 1] = ' ';
-
                             for (i = 0; i < t->ncolunas; i++)
                             {
                                 mvwprintw(janela, t->l_atual, i + 2, "%c", aux[i]);
                             }
-                            wmove(janela, t->l_atual, t->c_atual);
-                        break;
-
-                        case 27:
-                            attron(COLOR_PAIR(2));
-                            mvwchgat(janela, t->l_atual, 0, 2, 0, 1, NULL);
-                            attroff(COLOR_PAIR(2));
-                            wmove(janela, t->l_atual, 2);
-                            for (i = 0; i < t->ncolunas; i++)
-                            {
-                                mvwprintw(janela, t->l_atual, i + 2, "%c", tab[t->l_atual - 1][i]);
-                            }
-                            wmove(janela, t->l_atual, t->c_atual);
-                        break;
-
-                        case 10:
-                            attron(COLOR_PAIR(2));
-                            mvwchgat(janela, t->l_atual, 0, 2, 0, 1, NULL);
-                            attroff(COLOR_PAIR(2));
-                            wmove(janela, t->l_atual, t->c_atual);
-                        break;
-
-                        default:
-                            if(aux[t->ncolunas - 1] != ' '){
-                                noecho();
-                                break;
-                            }
-
-                            echo();
-                            for (i = t->ncolunas - 1; i >= t->c_atual; i--)
-                            {
-                                aux[i] = aux[i - 1];
-                            }
-                            aux[t->c_atual - 2] = tecla2;
-                            //t->n_palavras++;
-
-                            for (i = 0; i < t->ncolunas; i++)
-                            {
-                                mvwprintw(janela, t->l_atual, i + 2, "%c", aux[i]);
-                            }
-                            
-                            t->c_atual++;
-                            wmove(janela, t->l_atual, t->c_atual);
-                            break;
+                            wmove(janela, t->l_atual, t->c_atual - 1);
                         }
+                        break;
 
-                        if (tecla2 == 10)
+                    case KEY_DC:
+                        for (i = t->c_atual - 2; i < t->ncolunas; i++)
                         {
-                            for (i = 0; i < t->ncolunas; i++)
-                            {
-                                tab[t->l_atual - 1][i] = aux[i];
-                            }
+                            aux[i] = aux[i + 1];
+                        }
+                        aux[t->ncolunas - 1] = ' ';
+
+                        for (i = 0; i < t->ncolunas; i++)
+                        {
+                            mvwprintw(janela, t->l_atual, i + 2, "%c", aux[i]);
+                        }
+                        wmove(janela, t->l_atual, t->c_atual);
+                        break;
+
+                    case 27:
+                        attron(COLOR_PAIR(2));
+                        mvwchgat(janela, t->l_atual, 0, 2, 0, 1, NULL);
+                        attroff(COLOR_PAIR(2));
+                        wmove(janela, t->l_atual, 2);
+                        for (i = 0; i < t->ncolunas; i++)
+                        {
+                            mvwprintw(janela, t->l_atual, i + 2, "%c", tab[t->l_atual - 1][i]);
+                        }
+                        wmove(janela, t->l_atual, t->c_atual);
+                        break;
+
+                    case 10:
+                        attron(COLOR_PAIR(2));
+                        mvwchgat(janela, t->l_atual, 0, 2, 0, 1, NULL);
+                        attroff(COLOR_PAIR(2));
+                        wmove(janela, t->l_atual, t->c_atual);
+                        break;
+
+                    default:
+                        if (aux[t->ncolunas - 1] != ' ')
+                        {
+                            noecho();
                             break;
                         }
-                    }
-                    // envia a struct request para o server (para me retirar da tabela de editores)      
-                    write(inter_fifo_fd, &req, sizeof(req));
 
+                        echo();
+                        for (i = t->ncolunas - 1; i >= t->c_atual; i--)
+                        {
+                            aux[i] = aux[i - 1];
+                        }
+                        aux[t->c_atual - 2] = tecla2;
+
+                        for (i = 0; i < t->ncolunas; i++)
+                        {
+                            mvwprintw(janela, t->l_atual, i + 2, "%c", aux[i]);
+                            if (aux[i] != ' ')
+                                t->n_palavras++;
+                        }
+
+                        t->c_atual++;
+                        wmove(janela, t->l_atual, t->c_atual);
+                        break;
+                    }
+
+                    if (tecla2 == 10)
+                    {
+                        for (i = 0; i < t->ncolunas; i++)
+                        {
+                            tab[t->l_atual - 1][i] = aux[i];
+
+                        }
+                        break;
+                    }
+                }
+                strcpy(req.frase, aux);
+                // envia a struct request para o server (para me retirar da tabela de editores)
+                write(inter_fifo_fd, &req, sizeof(req));
+                close(inter_fifo_fd);
             } // fim da permissao
 
             noecho();
@@ -270,13 +275,9 @@ void criar_editor(WINDOW *janela, editor *t, char tab[t->nlinhas][t->ncolunas], 
         if (tecla == 27)
             break;
     }
-    close(c_fifo_fd);
-    close(inter_fifo_fd);
-    unlink(c_fifo_fname);
     wrefresh(janela);
     endwin(); // Encerra o ncurses
 }
-
 
 int main(int argc, char **argv)
 {
@@ -285,91 +286,97 @@ int main(int argc, char **argv)
     user u;
     server s;
     WINDOW *janela;
-    
-    char val_fifo_fname[20], inter_fifo_fname[20];
+
+    char val_fifo_fname[20], inter_fifo_fname[20], c_fifo_fname[20];
     valida val;
-    int val_fifo_fd, res, s_fifo_fd, r, w, inter_fifo_fd;
+    int c_fifo_fd, res, s_fifo_fd, r, w, inter_fifo_fd;
 
     // Verificar se o NP Servidor existe
-    if(access(SERVER_FIFO_P, F_OK) != 0) {
+    if (access(SERVER_FIFO_P, F_OK) != 0)
+    {
         fprintf(stderr, "\nServidor não está iniciado...!\n");
         exit(EXIT_FAILURE);
     }
+
     // abre o np do servidor
     s_fifo_fd = open(SERVER_FIFO_P, O_WRONLY);
-    if(s_fifo_fd == -1) {
+    if (s_fifo_fd == -1)
+    {
         fprintf(stderr, "\nO pipe principal do server não abriu!\n");
         exit(EXIT_FAILURE);
     }
 
-    sprintf(val_fifo_fname, VAL_FIFO, getpid());
+    // preenche o id para validacao
+    val.pid_user = getpid();
+    sprintf(c_fifo_fname, CLIENT_FIFO, val.pid_user);
 
-    // cria o pipe de validacao
-    res = mkfifo(val_fifo_fname, 0600);
-	if( res == -1 ) {
-		perror("\nmkfifo do FIFO do validacao deu erro");
-		exit(EXIT_FAILURE);
-	}
-	fprintf(stderr, "\nFIFO validacao criado");
-
-
-    // abre o pipe de validcao
-    val_fifo_fd = open(val_fifo_fname, O_RDWR);
-    if(val_fifo_fd == -1)
+    // cria o pipe
+    res = mkfifo(c_fifo_fname, 0600);
+    if (res == -1)
     {
-       fprintf(stderr,"\n O pipe val nao abriu\n");
-       close(s_fifo_fd);
-       exit(EXIT_FAILURE);
+        printf("\nErro ao criar o pipe do cliente!\n");
+        exit(EXIT_FAILURE);
     }
 
     getOption_cli(argc, argv, &u);
-    
-    if(strlen(u.nome) == 0)
+
+    if (strlen(u.nome) == 0)
     {
-        // preenche o nome do user 
-    	printf("\nInsira o nome de utilizador: ");
-	    scanf(" %7[^\n]", val.nome);
+        // preenche o nome do user
+        printf("\nInsira o nome de utilizador: ");
+        scanf(" %7[^\n]", val.nome);
     }
     else
         strcpy(val.nome, u.nome);
 
-    val.pid_user = getpid();
+    // abre o FIFO do cliente para escrita e leitura
+    c_fifo_fd = open(c_fifo_fname, O_RDWR);
+    if (c_fifo_fd == -1)
+    {
+        unlink(c_fifo_fname);
+        exit(EXIT_FAILURE);
+    }
 
     // manda a informacao ao servidor
     // para ser validada
     w = write(s_fifo_fd, &val, sizeof(val));
     if (w == sizeof(val))
-        fprintf(stderr, "\nEnviei [%d bytes]...", w);
+        fprintf(stderr, "\nValidacao bem enviada...\n", w);
 
     // le a informação recebida do servidor
     // validation info
-    r = read(val_fifo_fd, &val, sizeof(val));
-    if( r == sizeof(val))
-        fprintf(stderr,"\nli de val...");
+    r = read(c_fifo_fd, &val, sizeof(val));
+    if (r == sizeof(val))
+        fprintf(stderr, "\nValidacao bem lida...\n");
 
-    close(val_fifo_fd);
-    unlink(val_fifo_fname);
+    // USER PASSA A SER UM CLIENTE
 
     // se o utiizador for cliente
-    if(val.ver == 1)
+    if (val.ver == 1)
     {
         strcpy(inter_fifo_fname, val.np_name);
-        inter_fifo_fd = open(inter_fifo_fname, O_WRONLY);
+        /*inter_fifo_fd = open(inter_fifo_fname, O_WRONLY);
 
-        if(inter_fifo_fd == -1)
+        if (inter_fifo_fd == -1)
         {
-            fprintf(stderr,"\n O pipe val nao abriu\n");
+            fprintf(stderr, "\n O pipe val nao abriu\n");
             close(s_fifo_fd);
             exit(EXIT_FAILURE);
-        }
+        }*/
 
         inicia_vars(&t, &u, &s);
         char tab[t.nlinhas][t.ncolunas];
         strcpy(u.nome, val.nome);
-        criar_editor(janela, &t, tab, inter_fifo_fname);
+        /*printf("\n%s\n", val.np_name);
+        printf("\n%d\n", val.pid_user);
+        sleep(3);*/
+        criar_editor(janela, &t, tab, inter_fifo_fname, c_fifo_fd);
+        close(c_fifo_fd);
+        close(inter_fifo_fd);
+        unlink(c_fifo_fname);
     }
     else if (val.ver == 0)
-        fprintf(stderr,"\n '%s' nao consta na base de dados\n", val.nome);
+        fprintf(stderr, "\n '%s' nao consta na base de dados\n", val.nome);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -1,7 +1,15 @@
 #include "server.h"
 #include "structs.h"
 
-int main( int argc, char **argv) {
+void termina()
+{
+	printf("\nFUI MORTO!!!!\n");
+	unlink(SERVER_FIFO_P);
+	abort();
+}
+
+int main(int argc, char **argv)
+{
 
 	editor t;
 	user u;
@@ -11,20 +19,13 @@ int main( int argc, char **argv) {
 	// descritores dos pipes
 	int s_fifo_fd, c_fifo_fd, r, w, val_fifo_fd, inter_fifo_fd;
 	char c_fifo_fname[20], val_fifo_fname[20], inter_fifo_fname[20];
-	// strcuts
+	// structs
 	request req;
-	controlo ctrl;
 	valida val;
 	// variaveis controlo
-	int cfm, res , i;
+	int cfm, res, i;
 
-	int util = inter_pipes[0];
 	int pos = 0;
-
-	// inicia variaveis
-	// inicia_vars(&t, &u, &s);
-	// comandos adicionais do servidor
-	getOption_ser(argc, argv, &t, &u, &s);
 
 	// verfica se já existe servidor
 	if (access(SERVER_FIFO_P, F_OK) == 0)
@@ -32,22 +33,31 @@ int main( int argc, char **argv) {
 		fprintf(stderr, "\n Já existe um servidor!\n");
 		exit(EXIT_FAILURE);
 	}
+	printf("\nO meu pid: %d\n", getpid());
+	// inicia variaveis
+	inicia_vars(&t, &u, &s);
+	// comandos adicionais do servidor
+	getOption_ser(argc, argv, &t, &u, &s);
 
-	// inicializar o vetor com -1s
+	// inicializar o vetor com -1
 	for (i = 0; i < MAXLINES; i++)
 	{
 		editores[i] = -1;
 	}
 
-	// inicializar o vetor com 0s
+	// inicializar o vetor com 0
 	for (i = 0; i < MAXUSERS; i++)
 	{
 		inter_pipes[i] = 0;
 	}
 
+	int util = inter_pipes[0];
+	// Dá destino ao recebimento de um sinal SIGUSR1
+	signal(SIGUSR1, termina);
 	// cria o npipe do servidor
 	res = mkfifo(SERVER_FIFO_P, 0600);
-	if( res == -1 ) {
+	if (res == -1)
+	{
 		perror("\nmkfifo do FIFO do servidor deu erro");
 		exit(EXIT_FAILURE);
 	}
@@ -55,7 +65,8 @@ int main( int argc, char **argv) {
 
 	// abre o npipe do servidor
 	s_fifo_fd = open(SERVER_FIFO_P, O_RDWR);
-	if(s_fifo_fd == -1) {
+	if (s_fifo_fd == -1)
+	{
 		perror("\nErro ao abrir o FIFO do servidor (RDWR/bloqueante)");
 		exit(EXIT_FAILURE);
 	}
@@ -66,12 +77,12 @@ int main( int argc, char **argv) {
 	if (r == sizeof(val))
 		fprintf(stderr, "\nRecebi %d bytes do cliente ...", r);
 
-	// preenche o nome do pipe (val) com quem vai falar
-	sprintf(val_fifo_fname, VAL_FIFO, val.pid_user);
+	// preenche o nome do pipe com quem vai falar
+	sprintf(c_fifo_fname, CLIENT_FIFO, val.pid_user);
 
-	// abre o npipe de validacao para WR
-	val_fifo_fd = open(val_fifo_fname, O_WRONLY);
-	if(val_fifo_fd == -1)
+	// abre o npipe para WR
+	c_fifo_fd = open(c_fifo_fname, O_WRONLY);
+	if (c_fifo_fd == -1)
 	{
 		perror("\nErro ao abrir o FIFO de valicao");
 		close(s_fifo_fd);
@@ -79,19 +90,19 @@ int main( int argc, char **argv) {
 	}
 
 	// verifica se o username existe na base de dados
-	// verifica_user > return 1 se existir 
+	// verifica_user > return 1 se existir
 	// ERRO na abertura do ficheiro s->fich_nome (tem problemas)
 	val.ver = verifica_user(val.nome, &s);
 
 	// NAO PODE SER FEITO COM UM FOR PORQUE
 	// SENAO DEPOIS NAO CONSIGO DAR UNLINK
-	for(i = 0; i < MAXUSERS ; i++)
+	for (i = 0; i < MAXUSERS; i++)
 	{
-		sprintf(inter_fifo_fname, INTER_FIFO, i);		
+		sprintf(inter_fifo_fname, INTER_FIFO, i);
 		res = mkfifo(inter_fifo_fname, 0600);
-		if(res == -1)
+		if (res == -1)
 		{
-			fprintf(stderr,"\nErro ao abrir o pipe 'sss%d' de interação do servidor", i);
+			fprintf(stderr, "\nErro ao abrir o pipe 'sss%d' de interação do servidor", i);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -99,11 +110,12 @@ int main( int argc, char **argv) {
 	// FALTA FAZER ...
 	// named pipe para onde o cliente passa a falar
 
-	if(val.ver == 1)
-	{	
-		for (i=1 ; i < MAXUSERS; i++)
+	if (val.ver == 1)
+	{
+		for (i = 1; i < MAXUSERS; i++)
 		{
-			if(inter_pipes[i] < util) {
+			if (inter_pipes[i] < util)
+			{
 				util = inter_pipes[i];
 				pos = i;
 			}
@@ -112,9 +124,9 @@ int main( int argc, char **argv) {
 		sprintf(inter_fifo_fname, INTER_FIFO, pos);
 	}
 	inter_fifo_fd = open(inter_fifo_fname, O_RDWR);
-	if(inter_fifo_fd == -1)
+	if (inter_fifo_fd == -1)
 	{
-		fprintf(stderr,"\n O pipe val nao abriu\n");
+		fprintf(stderr, "\n O pipe val nao abriu\n");
 		close(s_fifo_fd);
 		close(val_fifo_fd);
 		exit(EXIT_FAILURE);
@@ -122,49 +134,53 @@ int main( int argc, char **argv) {
 	// copiar o nome do novo pipe para onde
 	// o cliente vai falar
 	strcpy(val.np_name, inter_fifo_fname);
-	
+
 	// escreve a informação da validacao no npipe
 	// de validacao
-	w = write(val_fifo_fd, &val, sizeof(val));
-	if ( w == sizeof(val))
-		fprintf(stderr,"\nEnviei [%d bytes] ao cliente",w);
-	
-	// fecha o pipe das validações
-	close(val_fifo_fd); 
+	w = write(c_fifo_fd, &val, sizeof(val));
+	if (w == sizeof(val))
+		fprintf(stderr, "\nEnviei [%d bytes] ao cliente", w);
 
-	while(1) {
+	close(c_fifo_fd);
+
+	while (1)
+	{
 		// le do pipe de interaçao
 		r = read(inter_fifo_fd, &req, sizeof(req));
-		if(r < sizeof(req)){
-			fprintf(stderr,"\nRecebido um request incompleto"
-							"\n[bytes lidos: %d]", r);
+		if (r != sizeof(req))
+		{
+			fprintf(stderr, "\nRecebido um request incompleto"
+							"\n[bytes lidos: %d]",
+					r);
 			continue;
-		}
-		fprintf(stderr, "\n> Numero de linha : [%d]\n", req.nr_linha);
-		
-		// VERIFICA SE A LINHA ESTA LIVRE ...	
-		for(i = 0; i<MAXLINES; i++){
-			
-			if(i == req.nr_linha) {
+		}else
+			fprintf(stderr, "\nrecebi a struct request\n");
 
-			if(editores[i] == req.pid_cliente)
+		fprintf(stderr, "\n> Numero de linha : [%d]\n", req.nr_linha);
+
+		// VERIFICA SE A LINHA ESTA LIVRE ...
+		for (i = 0; i < MAXLINES; i++)
+		{
+			if (i == req.nr_linha)
 			{
-				editores[i] = -1;
-				break;
-			}
-				if(editores[i] == -1)
+				if (editores[i] == req.pid_cliente)
+				{
+					editores[i] = -1;
+					break;
+				}
+				else if (editores[i] == -1)
 				{ // linha esta livre
 					editores[i] = req.pid_cliente;
-					ctrl.perm = 1;
+					req.perm = 1;
 				}
 				else // linha nao esta livre
-					ctrl.perm = 0;
+					req.perm = 0;
 			}
 		}
 
-		for(i = 0 ; i<MAXLINES; i++)
+		for (i = 0; i < MAXLINES; i++)
 		{
-			printf("\tLINHA: %d", i+1);
+			printf("\tLINHA: %d", i);
 			printf("\tEDITOR:%d\n", editores[i]);
 		}
 
@@ -172,20 +188,21 @@ int main( int argc, char **argv) {
 		sprintf(c_fifo_fname, CLIENT_FIFO, req.pid_cliente);
 
 		c_fifo_fd = open(c_fifo_fname, O_WRONLY);
-		if(c_fifo_fd == -1)
+		if (c_fifo_fd == -1)
 			perror("\nErro no open - Ninguem quis a resposta");
-		else {
-				fprintf(stderr, "\nFIFO cliente aberto para WRITE");
+		else
+		{
+			fprintf(stderr, "\nFIFO cliente aberto para WRITE");
 
-				w = write(c_fifo_fd, &ctrl, sizeof(ctrl));
-				if( w == sizeof(ctrl) )
-					fprintf(stderr,"\nescreveu a resposta");
-				else
-					perror("\nerro a escrever a resposta");
+			w = write(c_fifo_fd, &req, sizeof(req));
+			if (w == sizeof(req))
+				fprintf(stderr, "\nescreveu a resposta");
+			else
+				perror("\nerro a escrever a resposta");
 
-				close(c_fifo_fd);
-				fprintf(stderr,"\nFIFO cliente fechado");
-			}
+			close(c_fifo_fd);
+			fprintf(stderr, "\nFIFO cliente fechado");
+		}
 	}
 	close(s_fifo_fd);
 	unlink(SERVER_FIFO_P);
